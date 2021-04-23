8237f5e6670ab65d675149df7cd1a4331e2071b5
unknown <maktabisaid1@gmail.com>
2021-04-23 14:38:01 +0200

    integration html

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.index');
+})->name('index');
+
+Route::get('/about', function () {
+    return view('pages.about');
+})->name('about');
+
+Route::get('/products', function () {
+    return view('pages.products');
+})->name('products');
+
+Route::get('/store', function () {
+    return view('pages.store');
+})->name('store');
