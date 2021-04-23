cb3aa29977bafd7bbaed9a01914954104ba92082
unknown <maktabisaid1@gmail.com>
2021-04-23 15:35:47 +0200

    fin du projet

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,21 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('pages.index');
+    $currentPage = 'index';
+    return view('pages.index', compact('currentPage'));
 })->name('index');
 
 Route::get('/about', function () {
-    return view('pages.about');
+    $currentPage = 'about';
+    return view('pages.about', compact('currentPage'));
 })->name('about');
 
 Route::get('/products', function () {
-    return view('pages.products');
+    $currentPage = 'products';
+    return view('pages.products', compact('currentPage'));
 })->name('products');
 
 Route::get('/store', function () {
-    return view('pages.store');
+    $currentPage = 'store';
+    return view('pages.store', compact('currentPage'));
 })->name('store');
